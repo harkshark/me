0630b19b9c1ce02212a63788de4420aad0d685eb
Lauren Harkness <lauharkness@gmail.com>
2015-11-24 17:17:11 -0500

    Masonry Configured
    Added stylesheets and initialization, configured homepage.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+/* -----------------------------------------
+   My Scripts
+   github.com/harkshark/me
+   MIT License
+   By Lauren Harkness
+----------------------------------------- */
+
+
+/* Foundation --------------------------- */
+
 $(document).foundation();
+
+
+/* Masonry ------------------------------ */
+
+$(window).load(function(){
+
+	$('.masonry-house').masonry({
+		itemSelector: '.masonry-brick',
+		columnWidth:  240
+	});
+
+});//end .load
